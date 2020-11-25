f745f5c5b55ea01cb783b4cc20daedd7c306b55e
chris-brugal <chrisbrugal@yahoo.com>
2020-11-25 00:10:51 -0500

    quick sort

--- main.js
+++ main.js
@@ -9,4 +9,83 @@ fetch('city.list.json')
         } 
     });
 
-let test = 0;
+/*
+let myArray = [1, 2 ,3];
+function compareInts(a, b) {
+    return a - b;
+}
+
+function compareStrings(a, b) {
+    var nameA=a.name.toLowerCase(), nameB=b.name.toLowerCase()
+    if (nameA < nameB) //sort string ascending
+        return -1 
+    if (nameA > nameB)
+        return 1
+    return 0 //default return value (no sorting)
+}
+
+function selectionSort(arr, compareFunction) {
+    let len = arr.length;
+    for (let i = 0; i < len; i++) {
+        let min = i;
+        for (let j = i + 1; j < len; j++) {
+            //if (arr[min] > arr[j]) {
+            if (compareFunction(arr[min] > arr[j])) {
+                min = j;
+            }
+        }
+        if (min !== i) {
+            let tmp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = tmp;
+        }
+    }
+    return arr;
+}
+
+let myArray = [3, 2, 1]
+console.log(selectionSort(myArray, compareInts))
+*/
+
+function paritition(arr, low, high){
+    let pivot = arr[low];
+    let up = low;
+    let down = high;
+
+    while(up < down){
+        for(let i = up; i < high; i++){
+            if(arr[up] > pivot){
+                break;
+            }
+            up++;
+        }
+        for(let i = down; i > low; i--){
+            if(arr[down] < pivot){
+                break;
+            }
+            down++;
+        }
+        if(up < down){
+            swap(arr, up, down);
+        }
+    }
+    swap(arr, up, down);
+    
+}
+
+function swap(arr, i, j){
+    var temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+ }
+
+function quickSort (arr, low, high){
+    if (low < high){
+        let pivot = paritition(arr, low, high);
+        quickSort(arr, low, pivot - 1);
+        quickSort(arr, pivot + 1, high);
+    }
+    return arr;
+}
+
+
