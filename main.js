f8c81c40e9bf8d35b5eaf2d9a7f760a1d096030c
deguzmanc <deguzmanc@ufl.edu>
2020-11-30 21:14:54 -0700

    fixed weather description

--- main.js
+++ main.js
@@ -35,4 +35,22 @@ fetch('city.list.json')
 
 // console.log(quickSort(myArray))
 
+function getWeather(){ 
+    fetch("http://api.openweathermap.org/data/2.5/weather?q="+cityName+"&APPID=64c4674bb358ce6241ec014179c26214&units=imperial")
+    .then((response)=> response.json())
+    .then((data)=>{
+        console.log(data);
+        cityObj = data;
+        document.getElementById("displayCityName").innerHTML = data.name;
+        document.getElementById("displayTemp").innerHTML = data.main.temp + '°F';
+        document.getElementById("displayWeather").innerHTML = data.weather[0].description;
+        document.getElementById("displayCountryName").innerHTML = data.sys.country;
+        document.getElementById('flag').src = "https://www.countryflags.io/"+data.sys.country+"/flat/32.png";
+        document.getElementById("displayLong").innerHTML = "Longitutde: " + data.coord.lon;
+        document.getElementById("displayLat").innerHTML = "Latitude: " + data.coord.lat;
+        document.getElementById("displayFeelsLike").innerHTML = "Feels like: " + data.main.feels_like + '°F';
+        document.getElementById("displayHumid").innerHTML = "Humidity: " + data.main.humidity + "%";
+        document.getElementById("displayWindSpeed").innerHTML = "Wind Speed: " + data.wind.speed + " mph";
+    })  
+}
 
