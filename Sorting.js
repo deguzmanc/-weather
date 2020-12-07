93294c9eae534d267b4bf42558c1754e3ac9a0b8
deguzmanc <deguzmanc@ufl.edu>
2020-12-06 19:44:01 -0700

    fixed quicksort

--- Sorting.js
+++ Sorting.js
@@ -124,7 +124,8 @@ function heapSort(arr, sortby, ascending, city) {
 }
 
 function paritition(arr, low, high, compareFunction, city){
-    let pivot = arr[Math.floor((low + high) / 2)];//arr[low];
+    swap(arr, low, Math.floor((low + high) / 2));
+    let pivot = arr[low];
     let up = low;
     let down = high;
     
